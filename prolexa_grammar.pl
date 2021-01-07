88a58456342a814cf93cb4392b2f8620b875a206
Henry Addison <henryaddison@users.noreply.github.com>
2021-01-07 17:04:08 +0000

    grammer rule for negated verb phrase with non-proper noun as subject

--- prolexa_grammar.pl
+++ prolexa_grammar.pl
@@ -91,6 +91,7 @@ sentence1(C) --> determiner(N,M1,M2,C),noun(N,M1),verb_phrase(N,M2).
 sentence1([(L:-true)]) --> proper_noun(N,X),verb_phrase(N,X=>L).
 sentence1([not(L):-true]) -->  proper_noun(N,X),negated_verb_phrase(N,X=>L).
 sentence1([(L:-true)]) --> article(N), noun(N,X=>Lit), verb_phrase(N,P=>L), {Lit=..[P, X]}.
+sentence1([(not(L):-true)]) --> article(N), noun(N,X=>Lit), negated_verb_phrase(N,P=>L), {Lit=..[P, X]}.
 sentence1(C) --> conditional(C).
 sentence1(C) --> conditional2(C).
 sentence1([(VM:-AM,NM)]) --> adjective(N, X=>AM), noun(N, X=>NM), verb_phrase(N, X=>VM).
