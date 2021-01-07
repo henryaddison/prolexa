0d2095f6f7590c9b814bfcef24df9cbc0bfc4c4d
Henry Addison <henryaddison@users.noreply.github.com>
2021-01-07 17:19:04 +0000

    enable questions with non-proper nouns as subjects

--- prolexa_grammar.pl
+++ prolexa_grammar.pl
@@ -171,6 +171,7 @@ qword --> [].
 question1(Q) --> [who],verb_phrase(s,_X=>Q).
 question1(Q) --> [is], proper_noun(N,X),property(N,X=>Q).
 question1(Q) --> [does],proper_noun(_,X),verb_phrase(_,X=>Q).
+question1(Q) --> [is],article(N), noun(N,X=>Lit),property(N,P=>Q), {Lit=..[P, X]}.
 question1(has_ability(X, P)) --> [can],proper_noun(_,X),[do], noun(s, Y=>Lit), {Lit=..[P,Y]}.
 question1(has_ability(X, P)) --> [can],proper_noun(_,X), iverb(p, Y=>Lit), {Lit=..[P,Y]}.
 %question1((Q1,Q2)) --> [are,some],noun(p,sk=>Q1),
